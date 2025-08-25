dbbd85d60a17c83003b69a953a9bd7d6d660b8db
Anne-charlotte Gipson <anne-charlotte.gipson@epitech.eu>
2025-08-25 16:43:29 +0200

    integration page Landingpagepublic front

--- Gocrew_frontend/src/App.jsx
+++ Gocrew_frontend/src/App.jsx
@@ -1,28 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login.jsx";
 import Signup from "./Signup.jsx";
 import Forgot from "./Forgot.jsx";
-import ProtectedRoute from "./components/ProtectedRoute";
+import LandingPage from "./LandingPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-
     <BrowserRouter>
       <Routes>
-        {/* Redirection par défaut vers /login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        {/* Page d’accueil */}
+        <Route path="/" element={<LandingPage />} />
 
         {/* Pages publiques */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot" element={<Forgot />} />
 
+        {/* Redirection si la route n’existe pas */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
